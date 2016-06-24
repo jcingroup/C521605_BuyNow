2fcc9247c01eca26cf9af1c237d42e95eb5e1fbf
lei-chieh <lei.chieh@gmail.com>
2016-06-24 17:22:12 +0800

    後台排版調整

--- Work.WebProj/Scripts/src/tsx/m-community-news.tsx
+++ Work.WebProj/Scripts/src/tsx/m-community-news.tsx
@@ -552,8 +552,8 @@ namespace News {
                                         value={field.context} onChange={this.changeFDValue.bind(this, 'context') }></textarea>
                                 </div>
                             </div>
-                            <div className="form-group row form-action">
-                                <div className="col-xs-4 col-xs-offset-1">
+                            <div className="form-action">
+                                <div className="col-xs-offset-1">
                                     <button type="submit" className="btn btn-sm btn-primary"><i className="fa-check"></i> 儲存</button> { }
                                     <button type="button" className="btn btn-sm btn-secondary" onClick={this.noneType}><i className="fa-times"></i> 回前頁</button>
                                 </div>
