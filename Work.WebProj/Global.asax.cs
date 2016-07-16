1a9f87eb9ac36422d7852128a9761652f84a210a
Chain Fu <fusun@ms7.hinet.net>
2016-07-16 20:08:56 +0800

    set language

--- Work.WebProj/Global.asax.cs
+++ Work.WebProj/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using DotWeb.CommSetup;
 using System;
+using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -25,39 +26,61 @@ namespace DotWeb.AppStart
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
             HttpCookie WebLang = Request.Cookies[VarCookie + ".Lang"];
+            var set_lang = string.Empty;
+            string[] allow_lang = new string[] { "zh-TW" };
+            var fource_lang = string.Empty; //預設強制語系 
+            fource_lang = allow_lang[0]; //不預強制語系 此行註解
 
-            if (WebLang == null)
+            var query_lang = Request.QueryString["lang"]; //參數切換語系 參數查詢列為高優先權
+
+            if (!string.IsNullOrEmpty(query_lang) && allow_lang.Contains(query_lang))
             {
-                //強制預設語系
-                //WebLang = new HttpCookie(VarCookie + ".Lang", "zh-TW");
-                if (Request.UserLanguages != null)
-                    if (Request.UserLanguages.Length > 0)
-                        WebLang = new HttpCookie(VarCookie + ".Lang", Request.UserLanguages[0]);
-                    else
-                        WebLang = new HttpCookie(VarCookie + ".Lang", System.Threading.Thread.CurrentThread.CurrentCulture.Name);
-                else
-                    WebLang = new HttpCookie(VarCookie + ".Lang", System.Threading.Thread.CurrentThread.CurrentCulture.Name);
-
+                var n = System.Globalization.CultureInfo.CreateSpecificCulture(query_lang);//網址定語系
+                set_lang = n.Name;
+                WebLang = new HttpCookie(VarCookie + ".Lang", set_lang);
                 Response.Cookies.Add(WebLang);
             }
-
-            if (WebLang != null)
+            else if (WebLang == null)
             {
+                if (!string.IsNullOrEmpty(fource_lang))
+                {
+                    var q = fource_lang;
+                    var n = System.Globalization.CultureInfo.CreateSpecificCulture(q);//轉換完整 語系-國家 編碼
+                    set_lang = n.Name;
+                }
+                else if (Request.UserLanguages != null && Request.UserLanguages.Length > 0)
+                {
+                    var q = Request.UserLanguages[0];
+                    var n = System.Globalization.CultureInfo.CreateSpecificCulture(q);//轉換完整 語系-國家 編碼
 
-                if (WebLang.Value != "ja-JP" && WebLang.Value != "en-US")
-                {//強制預設語系只能為英文及日文
-                    HttpCookie setWebLang = new HttpCookie(DotWeb.CommSetup.CommWebSetup.WebCookiesId + ".Lang", "en-US");
-                    System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(setWebLang.Value);
-                    System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(System.Threading.Thread.CurrentThread.CurrentCulture.Name, false);
-                    Response.Cookies.Add(setWebLang); ;
+                    if (allow_lang.Contains(n.Name))
+                        set_lang = n.Name;
+                    else
+                        set_lang = allow_lang[0];
                 }
                 else
                 {
-                    System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(WebLang.Value);
-                    System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(System.Threading.Thread.CurrentThread.CurrentCulture.Name, false);
+                    var n = System.Threading.Thread.CurrentThread.CurrentCulture;
+                    if (allow_lang.Contains(n.Name))
+                        set_lang = n.Name;
+                    else
+                        set_lang = allow_lang[0];
                 }
-
+                WebLang = new HttpCookie(VarCookie + ".Lang", set_lang);
+                Response.Cookies.Add(WebLang);
             }
+            else
+            {
+                if (!allow_lang.Contains(WebLang.Value))
+                {
+                    set_lang = allow_lang[0];
+                    WebLang.Value = set_lang;
+                }
+            }
+
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(WebLang.Value);
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(System.Threading.Thread.CurrentThread.CurrentCulture.Name, false);
+
         }
     }
 }
